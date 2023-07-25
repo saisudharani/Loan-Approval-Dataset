a9284684d798537466f6cee2413a7e6158d97a28
saisudharani <saisudharani.mydam07@gmail.com>
2023-07-25 14:32:57 +0530

    sfg

---
+++ covert.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+class Numerical:
+    def converted(self,column,value):
+        df_train = pd.read_csv('numerical_data.csv')
+        cat_data = pd.read_csv('loan_train (1).csv')
+        num = df_train.loc[cat_data[cat_data[column]==value].index[0]][column]
+        return num
+
+
